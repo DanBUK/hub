e6a8168e88a277df1ff73b49be9216defbc4a35e
DanBUK <dan@f-box.org>
2011-07-17 21:19:42 +0100

    Adding example config.

---
+++ example_config.js
@@ -0,0 +1,13 @@
+module.exports.opts = {
+  hostname: 'hub.f-box.org',
+  port: 80,
+  session_key: 's3cr3t',
+  facebook: {
+    app_id: '12345',
+    app_secret: '1234567890'
+  },
+  twitter: {
+    consumer_key: '12345',
+    consumer_secret: '1234567890'
+  }
+}
